f3be6b775a29a4c36de9e37ef1437e51f2f35309
ChadYOYO <yoan.home71@gmail.com>
2026-05-02 22:36:23 +0200

    pages legales

--- supabase.js
+++ supabase.js
@@ -4,3 +4,12 @@ const SUPABASE_URL = 'https://bdwekhdyjimtvzflohan.supabase.co'
 const SUPABASE_KEY = 'sb_publishable_w8-gGy6dszRo-SL4N33_VQ_dhIWoB71'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+export async function getUser() {
+  const { data: { session } } = await supabase.auth.getSession()
+  return session?.user || null
+}
+
+export async function logout() {
+  await supabase.auth.signOut()
+  window.location.href = 'login.html'
+}
